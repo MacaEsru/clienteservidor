06d7f0685f0934548ecf065eb90cb88143bb202d
MacaEsru <163170@ids.upchiapas.edu.mx>
2020-01-23 21:01:21 -0600

    add settings

--- clienteservidor/settings.py
+++ clienteservidor/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q*^1lk42jbg=!qxine@p-0$5bjs$#8n5o5bv8suqo8cg)4dg9-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+MEDIA_URL = '/'
